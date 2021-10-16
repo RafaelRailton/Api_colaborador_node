297e6b8d7f6f5ca73dc190071eb2680cb31584a5
RafaelRailton <rafaelrailton@gmail.com>
2021-10-16 10:28:38 -0400

    feat: funcionalidade create colaborador

--- controllers/CollaboratorController.js
+++ controllers/CollaboratorController.js
@@ -1,4 +1,5 @@
 const { Validator } = require('node-input-validator')
+const CollaboratorModels = require('../models/Collaborator')
 class CollaboratorController {
 async create(req,res) {
     let {name,cpf,sector,password} = req.body;
@@ -8,12 +9,20 @@ async create(req,res) {
           sector: 'required|integer|min:1',
           password:'required|maxLength:10'
         })
- const matched = await v.check();
-  if (!matched) {
-    res.status = 422;
-    res.json(v.errors)
-    return;
-  }
+        const matched = await v.check();
+        if (!matched) {
+            res.status = 422;
+            res.json(v.errors)
+            return;
+        }
+       let result =  await CollaboratorModels.insert(name,cpf,sector,password);
+       if(result){
+           res.status = 200
+           res.json({'msg':'Cadastrado com sucesso!'})
+       }else{
+           res.status = 400;
+           res.json({'msg':`Error ${result}`})
+       }
 }
 }
 
--- models/Collaborator.js
+++ models/Collaborator.js
@@ -1,9 +1,14 @@
 const knex = require('../database/connection')
 class CollaboratorModels {
     async insert(name,cpf,sector,password){
-        
+        try {
+            let result = knex('collaborator').insert({cpf,name,sector,role:2,password})
+            return result;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
     }
 }
-
 module.exports = new CollaboratorModels();
 
